5af80ca0663924e830eae28365430e031992e1e3
sluka-alexander <slukasane@gmail.com>
2023-11-30 17:16:33 +0300

    Т-1 - started creating architecture of project

---
+++ src/app/shared/components/page-home/page-home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './page-home.component.html',
+  styleUrl: './page-home.component.less'
+})
+export class PageHomeComponent {
+
+}
